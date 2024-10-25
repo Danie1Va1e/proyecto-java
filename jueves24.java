66d3f33d655e827318eeb2e89b21d2eb92f6e9b5
DANIEL DEV <programdanielvale@gmail.com>
2024-10-24 20:12:53 -0400

    PRIMER CAMBIO DE UNA PRACTICA

---
+++ jueves24.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class jueves24
+{
+    public static Scanner leer= new Scanner(System.in);
+    
+    public static void main(){
+        
+      double numero;
+    
+      System.out.println("INGRESE UN NUMERO: ");   
+      numero= leer.nextInt();
+      
+      System.out.println(numero);
+      
+      
+    }
+    
+    
+}
