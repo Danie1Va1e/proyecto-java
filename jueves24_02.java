11bf6b6a8a100fd3e8798514571f2e8d1f21eb05
DANIEL DEV <programdanielvale@gmail.com>
2024-10-24 20:20:42 -0400

    SE GUARDO LA SEGUNDA MODIFICACION DE LA PRACTICA

---
+++ jueves24_02.java
@@ -0,0 +1,22 @@
+
+
+import java.util.Scanner;
+
+public class jueves24_02
+{
+    public static Scanner leer= new Scanner(System.in);
+    
+    public static void main(){
+        
+      String name;
+    
+      System.out.println("INGRESE UN NOMBRE: ");   
+      name= leer.next();
+      
+      System.out.println(name);
+      
+      
+    }
+    
+    
+}
